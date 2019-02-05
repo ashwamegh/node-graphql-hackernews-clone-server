0aa28c0879c7240b5c903389cdb6c762fbf7efda
Shashank Shekhar <shashank7200@gmail.com>
2019-02-06 00:22:07 +0530

    added query and mutation resolvers

--- src/index.js
+++ src/index.js
@@ -1,33 +1,70 @@
 const {GraphQLServer} = require('graphql-yoga');
+const path = require('path');
 
 
 // Schema definition
 
-const typeDefs = `
-  type Query {
-    info: String!
-    users: [User!]!
-    user(id: ID!) : User
-  }
-  type Mutation {
-    createUser(name: String!): User! 
-  }
+let links = [{
+  id: 'link-0',
+  description: "Some link to test" ,
+  url: 'www.howtographql.com'
+}]
 
-  type User {
-    id: ID!
-    name: String!
-  }
-  `;
+let idCount = links.length
 
 // Resolver
 const resolvers = {
   Query : {
-    info : () => 'This is the resolver for hackernews clne'
+    info : () => 'This is the resolver for hackernews clne',
+    feed : () => links,
+    link : (parent, args) => links.filter(item => item.id === args.id)
+  },
+
+  Mutation:{
+    post: (parent,args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link);
+
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      let deletedLink = {}
+      links = links.filter((link) =>{ 
+        if(link.id === args.id){
+          deletedLink = link
+        }
+        return link.id !== args.id 
+      })
+      return [deletedLink];
+    }, 
+
+    updateLink: (parent, args) => {
+      let updatedLink = {}
+      links = links.filter((link) => {
+        if(link.id === args.id){
+          updatedLink = {
+            id: link.id,
+            url: args.url ? args.url : link.url,
+            description: args.description ? args.description : link.description
+          }
+        }
+        return link.id !== args.id
+      })
+
+      links.push(updatedLink)
+
+      return [updatedLink]
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs, resolvers
+  typeDefs: path.resolve(__dirname,'./schema.graphql'), resolvers
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'));
