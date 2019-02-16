eb4044bf58c1f9496d3a9f5749f90f0f4c282d88
Shashank Shekhar <shashank7200@gmail.com>
2019-02-16 18:37:54 +0530

    updated queries and mutations using prisma client

--- src/index.js
+++ src/index.js
@@ -1,70 +1,65 @@
 const {GraphQLServer} = require('graphql-yoga');
 const path = require('path');
+// require('./prisma-demo');
+const { prisma } = require('./generated/prisma-client')
 
 
 // Schema definition
-
-let links = [{
-  id: 'link-0',
-  description: "Some link to test" ,
-  url: 'www.howtographql.com'
-}]
-
-let idCount = links.length
 
 // Resolver
 const resolvers = {
   Query : {
     info : () => 'This is the resolver for hackernews clne',
-    feed : () => links,
-    link : (parent, args) => links.filter(item => item.id === args.id)
+    feed : (root, args, context, info) => {
+      return context.prisma.links
+    },
+    link : (parent, args) => context.prisma.links.filter(item => item.id === args.id)
   },
 
   Mutation:{
-    post: (parent,args) => {
+    post: (parent,args, context, info) => {
       const link = {
-        id: `link-${idCount++}`,
         description: args.description,
         url: args.url
       }
 
-      links.push(link);
-
-      return link;
-    },
-    deleteLink: (parent, args) => {
-      let deletedLink = {}
-      links = links.filter((link) =>{ 
-        if(link.id === args.id){
-          deletedLink = link
-        }
-        return link.id !== args.id 
-      })
-      return [deletedLink];
-    }, 
-
-    updateLink: (parent, args) => {
-      let updatedLink = {}
-      links = links.filter((link) => {
-        if(link.id === args.id){
-          updatedLink = {
-            id: link.id,
-            url: args.url ? args.url : link.url,
-            description: args.description ? args.description : link.description
-          }
-        }
-        return link.id !== args.id
-      })
-
-      links.push(updatedLink)
-
-      return [updatedLink]
+      return context.prisma.createLink(link);
     }
+    // deleteLink: (parent, args) => {
+    //   let deletedLink = {}
+    //   links = links.filter((link) =>{ 
+    //     if(link.id === args.id){
+    //       deletedLink = link
+    //     }
+    //     return link.id !== args.id 
+    //   })
+    //   return [deletedLink];
+    // }, 
+
+    // updateLink: (parent, args) => {
+    //   let updatedLink = {}
+    //   links = links.filter((link) => {
+    //     if(link.id === args.id){
+    //       updatedLink = {
+    //         id: link.id,
+    //         url: args.url ? args.url : link.url,
+    //         description: args.description ? args.description : link.description
+    //       }
+    //     }
+    //     return link.id !== args.id
+    //   })
+
+    //   links.push(updatedLink)
+
+    //   return [updatedLink]
+    // }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs: path.resolve(__dirname,'./schema.graphql'), resolvers
+  typeDefs: path.resolve(__dirname,'./schema.graphql'), 
+  resolvers,
+  context: { prisma }
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'));
